ca60089468efbf8ed70b4859564b79f0d74020bb
Shane Jaroch <chown_tee@proton.me>
2023-11-14 12:20:11 -0500

    06-functions

--- src/lectures/exercise/functions/functions.go
+++ src/lectures/exercise/functions/functions.go
@@ -19,6 +19,32 @@ package main
 
 import "fmt"
 
-func main() {
+func hello(name string) {
+	fmt.Println("Hello", name)
+}
 
+func someMessage() string {
+	return "This is a super secret message!"
+}
+
+func sum3(a int, b int, c int) int {
+	return a + b + c
+}
+
+func someNumber() int {
+	return 235
+}
+
+func someNumbers2() [2]int {
+	return [2]int{85, 28}
+}
+
+func main() {
+	hello("Bob")
+	fmt.Println(someMessage())
+
+	fmt.Println(sum3(1, 3, 5))
+	fmt.Println(someNumber())
+	fmt.Println(someNumbers2())
+	fmt.Println(sum3(someNumber(), someNumbers2()[0], someNumbers2()[1]))
 }
